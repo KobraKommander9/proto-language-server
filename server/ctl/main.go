3e0b2710f655377f9f75d53fc1e3f8686b7f6ac0
Ed <ed.kilgore@tcnbroadcasting.com>
2023-02-07 23:36:37 -0700

    downgrade to go 1.18 and fix build issues

--- server/ctl/main.go
+++ server/ctl/main.go
@@ -1,27 +1,31 @@
 // Copyright 2023, Ed Kilgore
 //
-// This file is part of protobuf-language-server.
+// This file is part of proto-language-server.
 //
-// protobuf-language-server is free software: you can redistribute it and/or
+// proto-language-server is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// protobuf-language-server is distributed in the hope that it will be useful,
+// proto-language-server is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 // or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 // more details.
 //
 // You should have received a copy of the GNU General Public License along with
-// protobuf-language-server. If not, see <https://www.gnu.org/licenses/>.
+// proto-language-server. If not, see <https://www.gnu.org/licenses/>.
 
 // Package main defines the entry point to the server
 package main
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/KobraKommander9/proto-language-server/server/ctl/cmd"
 )
 
 func main() {
-	fmt.Printf("server started %v\n", n)
+	if err := cmd.RootCmd.Execute(); err != nil {
+		os.Exit(-1)
+	}
 }
