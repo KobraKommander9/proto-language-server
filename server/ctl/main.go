b0ca7f5e8317b0877a7e1102d97682642ade42db
Ed <ed.kilgore@tcnbroadcasting.com>
2023-02-07 22:50:12 -0700

    Add license

--- server/ctl/main.go
+++ server/ctl/main.go
@@ -1,19 +1,27 @@
+// Copyright 2023, Ed Kilgore
+//
+// This file is part of protobuf-language-server.
+//
+// protobuf-language-server is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// protobuf-language-server is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// protobuf-language-server. If not, see <https://www.gnu.org/licenses/>.
+
+// Package main defines the entry point to the server
 package main
 
 import (
 	"fmt"
-
-	"github.com/KobraKommander9/protobuf-language-server/server/ports/blank"
 )
 
 func main() {
-	var n blank.Client
-	n = Nothing{}
 	fmt.Printf("server started %v\n", n)
-}
-
-type Nothing struct{}
-
-func (Nothing) Close() error {
-	return nil
 }
